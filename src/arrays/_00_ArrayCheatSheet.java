a08ed65df889433b41334857a6b8f9dbe429e9bb
League Student <league@iMac-8.attlocal.net>
2018-08-11 17:31:34 -0700

    ArrayCheatSheet, RobotRace (IP)

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,23 +1,49 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
+		int small = Integer.MAX_VALUE;
+		int big = Integer.MIN_VALUE;
 		//1. make an array of 5 Strings
-		
+		String [] s = new String [5];
 		//2. print the third element in the array
-		
+		System.out.println("Before: " + s [2]);
 		//3. set the third element to a different value
-		
+		s [2] = "BEEEEEEEPPPPPPPPP!!!!!!";
 		//4. print the third element again
-		
+		System.out.println("After: " + s [2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		for(int i = 0; i<s.length; i++) {
+			System.out.println(s[i]);
+		}
 		
 		
 		//6. make an array of 50 integers
-		
+		int arr50 [] = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
+		for(int i = 0 ; i<arr50.length; i++) {
+			Random r = new Random();
+			arr50[i] = r.nextInt(200);
+		}
+		for(int i = 0; i<arr50.length; i++) {
+			if(arr50[i] < small) {
+				small = arr50[i];
+			}
+		}
+		System.out.println("Smallest: " + small);
 		
+		for(int i = 0; i<arr50.length; i++) {
+			if(arr50[i] > big) {
+				big = arr50[i];
+			}
+		}
+		System.out.println("Biggest: " + big);
+		for(int i = 0; i<arr50.length; i++) {
+			System.out.println(arr50 [i]);
+		}
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,17 +1,37 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
+	static int y = 0;
+	static boolean fin = false;
 	//1. make a main method
-
+	public static void main(String[] args) {
 	//2. create an array of 5 robots.
-
+		Robot [] rarr = new Robot [5];
 	//3. use a for loop to initialize the robots.
-
+	for(int i = 0; i<rarr.length; i++) {
+		rarr [i] = new Robot("batman");
+		rarr [i].setSpeed(50);
+		rarr [i].moveTo(0, y);
+		rarr [i].turn(90);
+		y += 100;
+	}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+	while(fin = false) {
+	for(int i = 0; i<rarr.length; i++) {
+		Random r = new Random();
+		rarr[i].move(r.nextInt(50));
+		if(rarr[i].getX() >= 800) {
+			fin = true;
+			System.out.println("And " + rarr [i] + " wins the race!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"); 
+		}
+	}}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -19,4 +39,4 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
-}
+}}
